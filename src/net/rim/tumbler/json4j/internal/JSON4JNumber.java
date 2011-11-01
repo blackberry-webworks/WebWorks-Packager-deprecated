dfcd5c31d9233e0868a36c269ade70ff3e31aceb
Nukul Bhasin <me@nukulb.com>
2011-11-01 13:07:52 -0400

    Initial move of the packager into its own repo.

---
+++ src/net/rim/tumbler/json4j/internal/JSON4JNumber.java
@@ -0,0 +1,11 @@
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://www.phonegap.com/about/license/ for full text.
+ *
+ * Copyright (c) 2011, IBM Corporation
+ */
+
+package net.rim.tumbler.json4j.internal;
+
+public class JSON4JNumber {
+}
